127df827b814b5fb4214c57ffc4aafd7237f2845
Jesslerchip <48423159+Jesslerchip@users.noreply.github.com>
2022-04-07 18:59:22 -0500

    Added entity file and updated main

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+damage = 1
+mob_hp = 5
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print("Mob appeared!")
+
+while mob_hp > 0:
+    print("Mob's HP: " + str(mob_hp))
+    attack = input("Attack?\n")
+    if attack.lower() == "attack":
+        mob_hp -= damage
+        print("Mob took " + str(damage) + " damage!")
+
+print("Mob defeated!")
