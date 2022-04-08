b970493c7522affc3674b0674d7e337d9c9052fa
Jesslerchip <48423159+Jesslerchip@users.noreply.github.com>
2022-04-07 19:45:43 -0500

    Added Player class.
    Added Player class.

--- entity.py
+++ entity.py
@@ -1,2 +1,29 @@
-class Player:
+max_exp = [0, 30, 60, 90, 120, 150]  # Array that sets players exp goals. WIP
 
+
+class Player:  # Creates the player instance
+
+    def __init__(self, name, player_class):  # Only takes player class because class determines base stats
+
+        self.name = name
+        self.player_class = player_class
+        self.hp = player_class[1]  # base HP from player class
+        self.mana = player_class[2]  # base mana from player class
+        self.stamina = player_class[3]  # base stamina from player class
+        self.power = player_class[4]  # base power from player class
+        self.defense = player_class[5]  # base defense from player class
+        self.speed = player_class[6]  # base speed from player class
+        self.player_perks = [0, 0, 0, 0, 0, 0, 0]  # perks towards each base stat (0 is critical chance, 1-6 are stats)
+        self.level = 1
+        self.xp = 0
+        self.max_exp = 30  # Set by max_exp array
+        self.crystals = 0
+
+    def set_stats(self):
+        self.hp = (self.player_class[1] + self.player_perks[1]) * self.level
+        self.mana = (self.player_class[2] + self.player_perks[2]) * self.level
+        self.stamina = (self.player_class[3] + self.player_perks[3]) * self.level
+        self.power = (self.player_class[4] + self.player_perks[4]) * self.level
+        self.defense = (self.player_class[5] + self.player_perks[5]) * self.level
+        self.speed = (self.player_class[6] + self.player_perks[6]) * self.level
+        self.max_exp = max_exp[self.level]
