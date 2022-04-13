66ec765acbc15778dfa8aa5a60d4e622489c342b
Sierra <sierrathomson03@gmail.com>
2022-04-13 18:34:41 -0400

    gui updated
    beginning mob implementation

--- gui.py
+++ gui.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import entity
+import mob_data
 import player_data
 
 
@@ -27,21 +28,55 @@ def make_player(name, player_class):
         my_label['text'] = "Invalid class."
 
 
+def hit(hp):
+    hp.set(hp.get() - 1)
+    if hp.get() <= 0:
+        button['state'] = DISABLED
+    return hp.get()
+
+
 def battle_gui(player):
     battle_main = Frame(main)
     battle_main.pack(padx=5, pady=5)
 
-    class_label = Label(battle_main, text="Class: " + player.player_class[0], fg="gray11")
-    class_label.grid(row=0, column=0, sticky=W)
+    p_name_label = Label(battle_main, text="Name: " + player.name, fg="gray11")
+    p_name_label.grid(row=0, column=0, sticky=W)
 
-    hp_label = Label(battle_main, text="hp: " + str(player.hp) + "/" + str(player.max_hp), fg="gray11")
-    hp_label.grid(row=0, column=1, sticky=W)
+    p_class_label = Label(battle_main, text="Class: " + player.player_class[0], fg="gray11")
+    p_class_label.grid(row=0, column=1, sticky=W)
 
-    mana_label = Label(battle_main, text="mana: " + str(player.mana) + "/" + str(player.max_mana), fg="gray11")
-    mana_label.grid(row=0, column=2, sticky=W)
+    p_hp_label = Label(battle_main, text="hp: " + str(player.hp) + "/" + str(player.max_hp), fg="gray11")
+    p_hp_label.grid(row=0, column=2, sticky=W)
 
-    mana_label = Label(battle_main, text="stamina: " + str(player.mana) + "/" + str(player.max_mana), fg="gray11")
-    mana_label.grid(row=0, column=3, sticky=W)
+    p_mana_label = Label(battle_main, text="mana: " + str(player.mana) + "/" + str(player.max_mana), fg="gray11")
+    p_mana_label.grid(row=0, column=3, sticky=W)
+
+    p_stamina_label = Label(battle_main, text="stamina: " + str(player.stamina) + "/" + str(player.stamina),
+                            fg="gray11")
+    p_stamina_label.grid(row=0, column=4, sticky=W)
+
+    new_mob = entity.Mob(1, mob_data.goblin)
+
+    m_name_label = Label(battle_main, text="Name: " + new_mob.name, fg="gray11")
+    m_name_label.grid(row=1, column=0, sticky=W)
+
+    m_hp_label = Label(battle_main, text="hp: " + str(new_mob.hp) + "/" + str(new_mob.max_hp), fg="gray11")
+    m_hp_label.grid(row=1, column=2, sticky=W)
+
+    m_mana_label = Label(battle_main, text="mana: " + str(new_mob.mana) + "/" + str(new_mob.max_mana), fg="gray11")
+    m_mana_label.grid(row=1, column=3, sticky=W)
+
+    m_stamina_label = Label(battle_main, text="stamina: " + str(new_mob.stamina) + "/" + str(new_mob.stamina),
+                            fg="gray11")
+    m_stamina_label.grid(row=1, column=4, sticky=W)
+
+    test_hp = IntVar()
+    test_hp.set(10)
+    button = Button(battle_main, text="Attack", command=lambda: test_hp.set(hit(test_hp)))
+    button.grid(row=3, column=2, padx=5, pady=5)
+
+    status_label = Label(battle_main, textvariable=test_hp, fg="green")
+    status_label.grid(row=2, column=2, padx=5, pady=5)
 
 
 main = Tk()
@@ -59,7 +94,8 @@ class_entry = Entry(mainframe)
 class_entry.pack(padx=5, pady=5)
 
 my_label = Label(mainframe, text="", fg="red")
-Button(mainframe, text="Submit", command=lambda: (make_player(name_entry.get(), class_entry.get()))).pack(padx=10, pady=10)
+Button(mainframe, text="Submit", command=lambda: (make_player(name_entry.get(), class_entry.get()))).pack(padx=10,
+                                                                                                          pady=10)
 my_label.pack()
 
 main.mainloop()
